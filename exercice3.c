928e978735e3873127e5e6f6002acd220e240df0
Jules COLIN <julesc5614029@gmail.com>
2020-01-13 12:30:32 +0100

    exercice3

--- exercice3.c
+++ exercice3.c
@@ -6,9 +6,12 @@ int pvJoueur;
 int pvMonstre;
 
 pvMonstre = 10;
+pvJoueur = 10;
 
+printf ("Joueur: %d PV \n", pvJoueur);
 printf ("Monstre: %d PV \n", pvMonstre);
-printf ("le joueur met un coup de point: Le Monstre perd 2 PV \n");
+printf("\n");
+printf ("le joueur met un coup de poing: Le Monstre perd 2 PV \n");
 
 pvMonstre = pvMonstre - 2;
 
@@ -19,6 +22,16 @@ printf("joueur attaque le monstre\n");
 printf("le monstre perd 2 PV\n");
 pvMonstre = pvMonstre - 2 ;
 printf("PV Monstre = %d PV\n",pvMonstre);
+printf("\n");
+if (pvMonstre < 6){
+printf("le monstre contre-attaque\n");
+printf("le monstre inflige 2 degats\n");
+pvJoueur = pvJoueur - 2;
+printf("PV joueur = %d\n", pvJoueur);
+printf("\n");
+}}
+if (pvMonstre <= 0){
+printf("Le Monstre est mort\n");
 }
 return 0;
 } 
